40b5f60229263ff5cfa3a06d6964caa21e9a5f63
Andro76as <andro76as@gmail.com>
2022-03-24 22:41:53 +0300

    Initial commit

---
+++ src/ru/netology/AndreyS/Education.java
@@ -0,0 +1,8 @@
+package ru.netology.AndreyS;
+
+public enum Education {
+    ELEMENTARY,
+    SECONDARY,
+    FURTHER,
+    HIGHER
+}
---
+++ src/ru/netology/AndreyS/Sex.java
@@ -0,0 +1,6 @@
+package ru.netology.AndreyS;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
